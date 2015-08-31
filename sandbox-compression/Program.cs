4ceb3c17572ae5745a01651b36af32b732b0223c
iburlakov <iburlakov@gmail.com>
2015-08-31 13:51:21 +0300

    empty project

---
+++ sandbox-compression/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Net;
+
+namespace sandboxcompression
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
